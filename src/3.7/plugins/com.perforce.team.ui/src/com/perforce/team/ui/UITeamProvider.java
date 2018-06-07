e6b566c944c2f6a237a8cc222858f5d13eb074e5
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2018-06-07 08:09:26 -0700

    Use singleton to avoid unnecessary refreshes in History view
    Also-by: Nicolas Kruk
    Also-by: Min Idzelis

--- src/3.7/plugins/com.perforce.team.ui/src/com/perforce/team/ui/UITeamProvider.java
+++ src/3.7/plugins/com.perforce.team.ui/src/com/perforce/team/ui/UITeamProvider.java
@@ -4,28 +4,29 @@
  */
 package com.perforce.team.ui;
 
+import org.eclipse.team.ui.history.IHistoryPageSource;
+
 import com.perforce.team.core.PerforceTeamProvider;
 import com.perforce.team.ui.history.P4HistoryPageSource;
 
-import org.eclipse.team.ui.history.IHistoryPageSource;
-
 /**
  * This class only use so that we can kick off ui plug-in
- * 
+ *
  * @author Sehyo Chang
- * 
+ *
  */
 public class UITeamProvider extends PerforceTeamProvider {
 
-    /**
-     * @see org.eclipse.team.core.RepositoryProvider#getAdapter(java.lang.Class)
-     */
+	private static P4HistoryPageSource historyPageSourceSingleton = new P4HistoryPageSource();
+
     @Override
-    public Object getAdapter(@SuppressWarnings("rawtypes") Class adapter) {
+    public <T> T getAdapter(Class<T> adapter) {
         if (IHistoryPageSource.class == adapter) {
-            return new P4HistoryPageSource();
+        	// use singleton to avoid unnecessary refreshes in History view
+            return adapter.cast(historyPageSourceSingleton);
         }
         return super.getAdapter(adapter);
     }
+
 
 }
