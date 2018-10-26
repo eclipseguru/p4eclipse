faa795884e8ecd0998588bd1d7e4ef4b87592e72
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2018-10-26 19:46:14 +0200

    Remove the prompt to edit files
    Because the action is async now, a sync dialog doesn't really make
    sense.

--- src/3.7/plugins/com.perforce.team.ui/src/com/perforce/team/ui/p4java/actions/EditAction.java
+++ src/3.7/plugins/com.perforce.team.ui/src/com/perforce/team/ui/p4java/actions/EditAction.java
@@ -3,25 +3,22 @@
  */
 package com.perforce.team.ui.p4java.actions;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.team.core.TeamException;
+
 import com.perforce.team.core.p4java.IP4File;
 import com.perforce.team.core.p4java.IP4Resource;
 import com.perforce.team.core.p4java.IP4Runnable;
 import com.perforce.team.core.p4java.P4Collection;
 import com.perforce.team.core.p4java.P4Runnable;
-import com.perforce.team.ui.P4ConnectionManager;
-
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.team.core.TeamException;
 
 /**
  * @author Kevin Sawicki (ksawicki@perforce.com)
  */
 public class EditAction extends OpenAction {
 
-	
+
     /**
      * @see com.perforce.team.ui.p4java.actions.P4Action#isEnabledEx()
      */
@@ -55,66 +52,7 @@ public class EditAction extends OpenAction {
 
     private void edit(final P4Collection collection, final int changelist,
             final String description, final boolean setActive) {
-    	    
-    	final boolean[] doNotEdit = new boolean [1];
-    	doNotEdit[0] = true;
-    	Display display = Display.getDefault();
-    	display.syncExec (new Runnable () {
-      
-    		public void run () {      	    	  
-    	    boolean unsyncedFiles = false;        	     	 
-    	    IP4Resource[] resources = collection.members();
-            int size = resources.length;
-            if (size > 0) {
-              for (IP4Resource resource : resources) {
-                  if (resource instanceof IP4File) {
-                      IP4File file = (IP4File) resource;
-                      if (!file.isSynced())
-                     	 unsyncedFiles = true;
-                  }
-              }
-              
-              if (unsyncedFiles == false)
-            	  doNotEdit[0] = false;
-          }
-                    
-          if (unsyncedFiles)          
-          {        	
-        	  String [] dialogButtonLabels = {Messages.EditAction_GetLatest, Messages.EditAction_DontGetLatest, Messages.EditAction_Cancel};
-        	  int buttonId=1; // default: don't get latest. This is matching test cases
-        	  if(!P4ConnectionManager.getManager().isSuppressErrors()){
-	        	  MessageDialog dlg = new MessageDialog(getShell(), 
-	        			  								Messages.EditAction_CheckOut, 
-	        			  								null, 
-	        			  								Messages.EditAction_UnsyncedFiles, 
-	        			  								MessageDialog.WARNING, 
-	        			  								dialogButtonLabels, 
-	        			  								0);           	        	 
-	        	  buttonId = dlg.open();
-        	  }
-        	  if (buttonId == 0)
-        		  {
-        		  	doNotEdit[0] = false;
-        		  	SyncAction action = new SyncAction();       	        		 
-        		  	action.setAsync(false);
-        		  	action.setCollection(getResourceSelection());
-        		  	action.runAction();
-        		  }  
-        	  else if (buttonId == 1)
-        	  {
-        		  doNotEdit[0] = false;
-        	  }
-        	  else if (buttonId == 2)
-        	  {
-        		  doNotEdit[0] = true;       		     	        	  
-        		  return;
-        	  }
-          }
-   }});
 
-    	if (doNotEdit[0] == true)        		
-    		return;
-    	
         IP4Runnable runnable = new P4Runnable() {
 
             @Override
